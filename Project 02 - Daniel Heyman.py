1a1097723d592422069526618e2e3ed8abd26760
Daniel Heyman <daniel.heyman@gmail.com>
2016-06-05 12:36:38 -0400

    Added ged file.

--- Project 02 - Daniel Heyman.py
+++ Project 02 - Daniel Heyman.py
@@ -2,7 +2,7 @@
 # CS 555
 # Project 02
 
-file = open('family.ged').read().splitlines()
+file = open('export-BloodTree.ged').read().splitlines()
 zero_tags = ["HEAD", "TRLR", "NOTE"]
 one_tags = ["NAME", "SEX", "BIRT", "DEAT", "FAMC", "FAMS", "MARR", "HUSB", "WIFE", "CHIL", "DIV"]
 two_tags = ["DATE"]
@@ -10,7 +10,7 @@ tags = [zero_tags, one_tags, two_tags]
 
 for line in file:
     print line
-    line = line.split(' ')
+    line = line.lstrip().rstrip().split(' ')
     print line[0]
     if len(line) == 3 and line[0] == '0' and line[2] in ["FAM","INDI"]:
         print line[2]
